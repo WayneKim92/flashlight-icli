b2d41976fd24600c9fcea45e284367a86e188eed
WayneKim92 <eunchan0817@gamil.com>
2023-08-23 16:36:20 +0900

    feat: logo 추가

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,23 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 import { init } from './init';
+import {showLogo} from "./showLogo";
 
 program
     .name('flashlight-icli')
     .description('Interactive command line interface for flashlight')
     .version('0.1.0')
+    .action(() => {
+        showLogo();
+        // feature 1 - 수동 성능 측정
+
+        // feature 2 - X번 자동으로 테스트하기
+
+        // feature 3 - report 선택해서 보기
+    })
 
 program.command('init')
     .description('init flashlight-icli')
     .action(async (_str, _options) => init());
-
-program.command('manual')
-    .description('simplest measure way')
-    .action(async (_str, _options) => {
-        console.log('가장 간단한 측정 방법')
-    });
-
-program.command('test')
-    .description('performance and report')
-    .action(async (_str, _options) => {
-        console.log('가장 간단한 측정 방법')
-    });
-
-program.command('reports')
-    .description('show reports')
-    .action(async (_str, _options) => {
-        console.log()
-    });
 
 program.parse();
